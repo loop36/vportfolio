1be50489bd12e2ef7eae2531008ea6c8291fdbbf
Anand Balagopalan <anand.b@qburst.com>
2023-10-30 12:48:08 +0530

    Added Responsive - main container UI changes

--- src/components/HeroContent/HeroContent.tsx
+++ src/components/HeroContent/HeroContent.tsx
@@ -2,9 +2,9 @@ import { Button, Grid, Typography } from "@mui/material";
 import "./HeroContent.scss";
 const HeroContent = () => {
   return (
-    <Grid item alignItems={"flex-end"} display={"flex"} marginBottom={4} maxWidth={'40%'}>
+    <Grid item alignItems={"flex-end"} display={"flex"} marginBottom={4}>
       <div className="hero_container">
-        <Typography variant="h6" className="hero_title" fontWeight={700}>
+        <Typography variant="h6" className="hero_title" fontWeight='bold' fontSize={24}>
           Business Analyst
         </Typography>
         <Typography paragraph maxWidth={460}>
@@ -16,14 +16,14 @@ const HeroContent = () => {
           <Button
             variant="outlined"
             className="action_button"
-            sx={{ borderRadius: 40, color: "black", fontWeight: 600 , paddingInline:4, fontSize:14}}
+            sx={{ borderRadius: 40, color: "black", fontWeight: 600, paddingInline: 4, fontSize: 14 }}
           >
             Download Resume
           </Button>
           <Button
             variant="outlined"
             className="action_button"
-            sx={{ borderRadius: 40, color: "black", fontWeight: 600 ,paddingInline:4 ,fontSize:14}}
+            sx={{ borderRadius: 40, color: "black", fontWeight: 600, paddingInline: 4, fontSize: 14 }}
           >
             Connect With Me
           </Button>
